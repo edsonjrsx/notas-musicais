1057aa62a16e984490ace9742411d50278b505ab
Edson Carvalho <junior.edson@unifesp.br>
2025-10-27 22:38:36 -0300

    chore: mundando linter para ruff

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -9,20 +9,21 @@ def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
     Parameters:
         tonica (str): A nota tônica da escala
         tonalidade (str): A tonalidade da escala
-        
+
     Returns:
         Um dicionário contendo as notas e os graus da escala
-        
+
     Examples:
         >>> escalas('C', 'maior')
         {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
-        
+
         >>> escalas('A', 'maior')
         {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
     """
-    intervalos = ESCALAS[tonalidade]
+    intervalos=ESCALAS[tonalidade]
     tonica_pos = NOTAS_MUSICAIS.index(tonica)
     tmp_array = []
+
     for intervalo in intervalos:
         nota = (tonica_pos + intervalo) % 12
         tmp_array.append(NOTAS_MUSICAIS[nota])
